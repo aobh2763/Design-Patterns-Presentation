8b7c58b3ff8264605285353357c106f0577f4466
ahmedomarbenhazem <benhazem.ahmedomar@gmail.com>
2025-11-21 17:01:07 +0100

    fixed SOME css + structure

--- observer/src/main.ts
+++ observer/src/main.ts
@@ -3,6 +3,8 @@ import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/black.css";
 
+import "./style.css";
+
 let deck = new Reveal({
   plugins: [Markdown],
 });
