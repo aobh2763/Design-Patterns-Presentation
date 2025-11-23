74a982f8429a5614c03d8ccd9aab62f1ed8e7ca2
ahmedomarbenhazem <benhazem.ahmedomar@gmail.com>
2025-11-23 21:39:48 +0100

    done

--- observer/src/main.ts
+++ observer/src/main.ts
@@ -1,11 +1,18 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+
 import "reveal.js/dist/reveal.css";
-import "reveal.js/dist/theme/black.css";
+import "reveal.js/dist/theme/blood.css";
+import "reveal.js/plugin/highlight/monokai.css";
 
 import "./style.css";
 
 let deck = new Reveal({
-  plugins: [Markdown],
+  plugins: [Markdown, Highlight],
 });
-deck.initialize();
+deck.initialize({ 
+    margin: 0,
+    width: "100%",
+    height: "100%",
+});
